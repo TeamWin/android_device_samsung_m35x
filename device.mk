1207a72061e9b2ddef6380a09574aa301bfc7c98
mrfluffyoven <mrfluffyoven@gmail.com>
2025-12-14 09:05:33 +0000

    Update Fastboot to V1.1 & add zip flashing
    Change-Id: I990b8a77b6ae58791dbaa8f0f4fe051d6f4e6a9d

--- device.mk
+++ device.mk
@@ -12,5 +12,5 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
