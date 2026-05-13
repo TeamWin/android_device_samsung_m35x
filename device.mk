8b0cf1839de1df93921314987a905030a5f493d8
SavedByLight <mrfluffyoven@gmail.com>
2026-05-13 11:26:33 +0100

    cleanup tree

--- device.mk
+++ device.mk
@@ -19,7 +19,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
 
 # sideload
 PRODUCT_PACKAGES += \
